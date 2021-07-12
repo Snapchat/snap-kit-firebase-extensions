ac5ddbadcb362d8ef24aff02d8aae80944dcbe8a
Hongjai Cho <hcho@snapchat.com>
2021-07-12 19:13:32 -0400

    Fix issues found from running npm run lint

--- login-kit/functions/src/config.ts
+++ login-kit/functions/src/config.ts
@@ -3,9 +3,9 @@
  */
 
 export default {
-    clientId: process.env.CLIENT_ID,
-    signingServiceAcocunt: process.env.SIGNING_SERVICE_ACCOUNT,
+  clientId: process.env.CLIENT_ID,
+  signingServiceAcocunt: process.env.SIGNING_SERVICE_ACCOUNT,
 
-    authServiceUrl: "https://accounts.snapchat.com",
-    accessTokenEndpointPath: "/accounts/oauth2/token"
-}
+  authServiceUrl: "https://accounts.snapchat.com",
+  accessTokenEndpointPath: "/accounts/oauth2/token",
+};
